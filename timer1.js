60d559312bf4a83ac26c3bc9369b659b91f262d9
originallykevin <ly1@ualberta.ca>
2022-08-31 00:16:19 +0000

    refactor with code review

--- timer1.js
+++ timer1.js
@@ -1,11 +1,14 @@
-let input = process.argv.slice(2);
-input = input.filter(time => time > 0).filter(Number);
-// input = input.filter(Number) // move this code to above
-console.log(input);
+// refactor after code review
+const input = process.argv.slice(2).filter(time => (!isNaN(Number(time)) && time > 0));
 
-let count = 0;
+// break down code below(old code)
+// let input = process.argv.slice(2)
+// input.filter(time => time > 0));
+// input = input.filter(Number) 
+// console.log(input);
+
 for (const time of input) {
   setTimeout((time) => {
     process.stdout.write('. \n'); // create new line per 'beep'
-  }, count += time * 1000); // *1000 to convert input into seconds
+  }, time * 1000); // *1000 to convert input into seconds
 }
